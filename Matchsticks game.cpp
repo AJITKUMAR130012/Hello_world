bbbb84e039b7cc7ccc88957e22e7a86c01181ad1
Ajit Kumar <60688738+AJITKUMAR130012@users.noreply.github.com>
2021-10-08 18:08:00 +0530

    Create Matchsticks game.cpp

---
+++ Matchsticks game.cpp
@@ -0,0 +1,8 @@
+//https://practice.geeksforgeeks.org/problems/-matchsticks-game4906/1
+
+int matchGame(long long N) {
+        // code here
+        int n=N%5;
+        if(n==0) return -1;
+        else return n;
+    }
