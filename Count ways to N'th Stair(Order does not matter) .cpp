c0af2e4d1ecff1e3899da3c8ca7a7cd4eeec6f9d
Ajit Kumar <60688738+AJITKUMAR130012@users.noreply.github.com>
2021-10-08 18:14:03 +0530

    Create Count ways to N'th Stair(Order does not matter) .cpp

---
+++ Count ways to N'th Stair(Order does not matter) .cpp
@@ -0,0 +1,7 @@
+//https://practice.geeksforgeeks.org/problems/count-ways-to-nth-stairorder-does-not-matter1322/1
+
+long long countWays(int n)
+    {
+        // your code here
+        return n/2 +1;
+    }
