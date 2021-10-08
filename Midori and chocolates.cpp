b8178d083cbd540425613ef4a8644dc0df4aec74
Ajit Kumar <60688738+AJITKUMAR130012@users.noreply.github.com>
2021-10-08 17:51:39 +0530

    Create Midori and chocolates.cpp

---
+++ Midori and chocolates.cpp
@@ -0,0 +1,6 @@
+//https://practice.geeksforgeeks.org/problems/midori-and-chocolates2438/1
+
+long long int leftShops(long long int i, long long int L){
+        // code here
+        return (1<<L)-i;
+    }
